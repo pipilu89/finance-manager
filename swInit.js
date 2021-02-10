4c9bddc2eda69f35781d091514a1b5dc8fafbbcc
pipilu <pipilu89@outlook.com>
2021-02-10 21:03:47 +0800

    ./service-worker.js

--- swInit.js
+++ swInit.js
@@ -2,7 +2,7 @@
 //inspect sw from within chrome: chrome://inspect/#service-workers
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("service-worker.js").then(
+    navigator.serviceWorker.register("./service-worker.js").then(
       // navigator.serviceWorker.register("/service-worker.js").then(
       function (registration) {
         // Registration was successful
